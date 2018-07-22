e0b95f98333446d9e041b7fb0690da9c2e3ccac7
Akuoma Cyril-Obi <akuomacyrilobi@akuomas-macbook-pro.local>
2018-07-22 18:01:48 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Akuoma Cyril-Obi on 22/07/2018.
+//  Copyright © 2018 Akuoma Cyril-Obi. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
